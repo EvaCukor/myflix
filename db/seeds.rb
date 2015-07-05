f67721f42452933c0a036f259ff6e36bc8123bdc
evacukor <evacukor@gmail.com>
2015-07-05 14:46:12 +0000

    added video reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,13 +15,13 @@ Video.create!(title: 'Family Guy',
              large_cover_url: 'family_guy_large.jpg',
              category: comedy)
 
-Video.create!(title: 'Futurama',
+futurama = Video.create!(title: 'Futurama',
              description: 'Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year\'s Eve 2999.',
              small_cover_url: 'futurama.jpg',
              large_cover_url: 'futurama_large.jpg',
              category: sci_fi)
 
-Video.create!(title: 'Monk',
+monk = Video.create!(title: 'Monk',
              description: 'Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.',
              small_cover_url: 'monk.jpg',
              large_cover_url: 'monk_large.jpg',
@@ -32,3 +32,8 @@ Video.create!(title: 'Monk',
              small_cover_url: 'family_guy.jpg',
              large_cover_url: 'family_guy_large.jpg',
              category: comedy)}
+             
+eva = User.create(full_name: 'Eva Cukor', password: 'password', email: 'cukor@email.com')
+
+Review.create(user: eva, video: monk, rating: 3, contents: 'This is a really good TV show.')
+Review.create(user: eva, video: futurama, rating: 5, contents: 'This is a very funny TV show!')
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -13,6 +13,16 @@ describe VideosController do
     #   get :show, id: video.id
     #   expect(response).to render_template :show
     # end
+    
+    it "sets @reviews for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      review1 = Fabricate(:review, video: video)
+      review2 = Fabricate(:review, video: video)
+      get :show, id: video.id
+      expect(assigns(:reviews)).to match_array([review1, review2])
+      #assigns(:reviews).should =~ [review1, review2]
+    end
 
     it "redirects to the signin page for unauthenticated users" do
       video = Fabricate(:video)
