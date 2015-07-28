97b13fd2a818775d5e52a4ff9c639759b2afef89
evacukor <evacukor@gmail.com>
2015-07-28 09:13:54 +0000

    added authentication tests, video reviews and corresponding tests

--- db/schema.rb
+++ db/schema.rb
@@ -23,10 +23,12 @@ ActiveRecord::Schema.define(version: 20150705142312) do
   end
 
   create_table "reviews", force: true do |t|
-    t.integer "user_id"
-    t.integer "video_id"
-    t.text    "contents"
-    t.integer "rating"
+    t.integer  "user_id"
+    t.integer  "video_id"
+    t.text     "contents"
+    t.integer  "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
